577350c4eeb5422d8c9b70149e7b0cb9068b4060
Carlos Daniel Freire Fernandes <carloscdanield@gmail.com>
2022-09-24 11:19:11 -0300

    add aula 01

---
+++ ImersaoDevAlura/Aula01-Variaveis-Operacoes-Media/CalculadoraDeMedia/script.js
@@ -0,0 +1,16 @@
+var nome = "Daniel"
+
+var n1 = 9
+var n2 = 7
+var n3 = 4
+var n4 = 2
+
+var notaFinal = (n1 + n2 + n3 + n4) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log ("Bem Vindo " + nome)
+console.log (notaFixada)
+
+//REVISÃO
+//Variaveis, strings, console.log, toFixed, operações matemáticas, concatenação.
